1cc20e876196605d1ae4f71ba7bdc8187e233abb
evanbones <74276616+evanbones@users.noreply.github.com>
2026-04-02 12:45:48 -0700

    Fixes and adjustments

--- common/src/main/java/com/evandev/treeliable/mixin/BlockStateMixin.java
+++ common/src/main/java/com/evandev/treeliable/mixin/BlockStateMixin.java
@@ -4,7 +4,10 @@ import com.evandev.treeliable.api.TreeData;
 import com.evandev.treeliable.client.Client;
 import com.evandev.treeliable.common.chop.ChopUtil;
 import com.evandev.treeliable.common.config.ModConfig;
+import com.evandev.treeliable.common.settings.ChopSettings;
+import com.evandev.treeliable.platform.server.Server;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -23,13 +26,29 @@ public abstract class BlockStateMixin {
         if (blockGetter instanceof Level level && ChopUtil.isBlockChoppable(level, pos)) {
             BlockState state = blockGetter.getBlockState(pos);
 
+            ChopSettings settings;
+            if (level instanceof ServerLevel) {
+                settings = Server.instance().getPlayerChopData(player).getSettings();
+            } else {
+                settings = Client.getChopSettings();
+            }
+
+            if (!ChopUtil.playerWantsToChop(player, settings)) {
+                return;
+            }
+
             if (!ChopUtil.canChopWithTool(player, player.getMainHandItem(), level, pos, state)) {
                 return;
             }
 
-            TreeData tree = Client.treeCache.getTree(level, pos);
+            TreeData tree;
+            if (level instanceof ServerLevel) {
+                tree = ChopUtil.getTree(level, pos);
+            } else {
+                tree = Client.treeCache.getTree(level, pos);
+            }
 
-            if (tree != null && tree.isAProperTree(true)) {
+            if (tree != null && tree.isAProperTree(settings.getTreesMustHaveLeaves())) {
                 if (ModConfig.get().hytaleLikeFelling) {
                     return;
                 }
